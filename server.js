d49330864047871e7dff75fcd2c72858cdfb0ae7
Datior <leonardowederveiga@yahoo.com>
2023-03-15 17:21:25 -0300

    trabalhando com express

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-const http = require('http')
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de node',
-    '/livros': 'Pagina de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Pagina de editora',
-    '/sobre': 'Info sobre o projeto'
-}
-    
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})  // cabecalho que devolve um STATUS http, e um conteudo
-    res.end(rotas[req.url]) // texto que quero enviar de acordo com a URL da minha requisição
-})
-server.listen(port, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
+
